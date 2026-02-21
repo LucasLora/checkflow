dcfbcc9e214a4ecabed9beec96e544d80410fae4
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2026-02-21 14:03:24 -0300

    Bugfix - Delete photo deletes photos in other item (#12)
    * Add uuid package
    
    * Fix adding uuid to filename

--- lib/features/checklists/services/image_service.dart
+++ lib/features/checklists/services/image_service.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class ImageService {
   final ImagePicker _picker = ImagePicker();
+  final Uuid _uuid = Uuid();
 
   Future<String?> takePhoto() {
     return _pickAndSave(ImageSource.camera);
@@ -24,11 +26,13 @@ class ImageService {
     if (image == null) return null;
 
     final directory = await getApplicationDocumentsDirectory();
-    final fileName = p.basename(image.path);
+
+    final extension = p.extension(image.path);
+    final uniqueName = '${_uuid.v4()}$extension';
 
     final savedImage = await File(
       image.path,
-    ).copy('${directory.path}/$fileName');
+    ).copy('${directory.path}/$uniqueName');
 
     return savedImage.path;
   }
